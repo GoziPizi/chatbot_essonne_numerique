c5e759a93c56cbb14c7961eeac47c42498a11edf
gozipizi <quentin.gourdel@ensiie.fr>
2025-04-17 09:56:29 +0200

    Remove phone

--- src/app/forms/fd-form/fd-form.component.ts
+++ src/app/forms/fd-form/fd-form.component.ts
@@ -1,5 +1,11 @@
 import { Component, ViewChild } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { ImageInputComponent } from '../image-input/image-input.component';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { FormDisplayService } from '../../services/form-display.service';
@@ -9,31 +15,31 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-fd-form',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule, NgSelectModule, ImageInputComponent, CommonModule],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    NgSelectModule,
+    ImageInputComponent,
+    CommonModule,
+  ],
   templateUrl: './fd-form.component.html',
-  styleUrl: './fd-form.component.scss'
+  styleUrl: './fd-form.component.scss',
 })
 export class FdFormComponent {
-
-  cities = cities
-  @ViewChild('picture') pictureInput: ImageInputComponent | undefined
+  cities = cities;
+  @ViewChild('picture') pictureInput: ImageInputComponent | undefined;
 
   fdForm = new FormGroup({
     name: new FormControl('', [Validators.required]),
     firstname: new FormControl('', [Validators.required]),
     mail: new FormControl('', [Validators.required]),
-    phone: new FormControl('', [Validators.required]),
     address: new FormControl('', [Validators.required]),
     city: new FormControl(null, [Validators.required]),
     risk: new FormControl('', Validators.required),
-    comment: new FormControl('', [Validators.required])
-  })
+    comment: new FormControl('', [Validators.required]),
+  });
 
-  constructor(
-    private formService: FormDisplayService
-  ) {
-
-  } 
+  constructor(private formService: FormDisplayService) {}
 
   getForm() {
     return this.fdForm.value;
@@ -50,5 +56,4 @@ export class FdFormComponent {
   get isDisabled() {
     return this.fdForm.invalid || !this.pictureInput?.isImageValid;
   }
-
 }
